b083c8ae8f6ef2244c799d206f123a49d88eeb9f
vbharathkumar2003 <bharathvenkat03@gmail.com>
2024-05-23 12:37:04 +0530

    responsiveness added

--- src/Admin/RoundedImages.js
+++ src/Admin/RoundedImages.js
@@ -1,34 +1,50 @@
 // RoundedImages.js
 
-import React from 'react';
-import { Link } from 'react-router-dom';
-import StaffImage from './assets/Staffimage.jpeg';
-import StudentImage from './assets/Studentimage.jpeg';
+import React from "react";
+import { Link } from "react-router-dom";
+import StaffImage from "./assets/Staffimage.jpeg";
+import StudentImage from "./assets/Studentimage.jpeg";
 
 const RoundedImages = () => {
   return (
     <div className="rounded-images-container">
-      <div className="rounded-image">
-        <Link to="/App3/addstaff" style={{ textDecoration: 'none', color: 'inherit' }}>
-          <img src={StaffImage} alt="RoundedImage 1" />
-          <p>STAFF</p>
-        </Link>
-      </div>
-      <div className="rounded-image">
-        <Link to="/App3/addstudent" style={{ textDecoration: 'none', color: 'inherit' }}>
-          <img
-            src={StudentImage}
-            alt="RoundedImage 2"
-            style={{
-              borderRadius: '50%',
-              width: '115px',
-              height: '115px',
-              objectFit: 'cover',
-              marginBottom: '5px',
-            }}
-          />
-          <p style={{ marginTop: '7px' }}>STUDENT</p>
-        </Link>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          // justifyContent: "center",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+      >
+        <div>
+          <Link
+            to="/App3/addstaff"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <img src={StaffImage} alt="RoundedImage 1" />
+            <p>STAFF</p>
+          </Link>
+        </div>
+        <div>
+          <Link
+            to="/App3/addstudent"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <img
+              src={StudentImage}
+              alt="RoundedImage 2"
+              // style={{
+              //   borderRadius: "50%",
+              //   width: "115px",
+              //   height: "115px",
+              //   objectFit: "cover",
+              //   marginBottom: "5px",
+              // }}
+            />
+            <p>STUDENT</p>
+          </Link>
+        </div>
       </div>
     </div>
   );
